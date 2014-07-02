05ad79e04b2a5c2651b23db49ed72e63e5bcc9e2
neoinsanity <mindbender@gmail.com>
2014-07-02 15:22:34 -0700

    Initial setup.

---
+++ samples/ook_vedis/setup.py
@@ -0,0 +1,15 @@
+"""The setuptools setup file.
+
+"""
+
+from setuptools import find_packages, setup
+
+setup(
+    name='ook_vedis',
+    version='0.0.0.x',
+    license='Apache License 2.0',
+    description='A need to remember.',
+    packages=['ook-vedis',],
+    install_requires=[],
+    include_package_data = True,
+)
